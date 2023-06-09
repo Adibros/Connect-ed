704b2f44fb666eec0d6844d961a9dd30f1f20b81
Adibros <86347816+Adibros@users.noreply.github.com>
2023-06-09 07:09:16 +0530

    building login ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <Router>
       <Header/>
+
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
     </Router>
   );
 }
