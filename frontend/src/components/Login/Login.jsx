09f7d92e93515d28f45e5e9c045d03762d8e4304
Adibros <86347816+Adibros@users.noreply.github.com>
2023-06-14 06:40:23 +0530

    login request- client

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Login.css'
 import { Typography,Button} from "@mui/material"
 import {Link} from "react-router-dom"
+import {useDispatch} from "react-redux";
+import { loginUser } from '../../Actions/User';
 
 const Login = () => {
+
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    dispatch(loginUser(email,password));
+  }
+
   return (
     <div className='login'>
 
-        <form className='loginForm'>
+        <form className='loginForm' onSubmit={loginHandler}>
 
             <Typography variant='h3' style={{padding:'2vmax'}}>Connect-ed</Typography>
-            <input type="email" placeholder='Email' required/>
-            <input type="password" placeholder='Password' required/>
+
+            <input 
+              type="email" 
+              placeholder='Email' 
+              required 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input 
+              type="password" 
+              placeholder='Password' 
+              required 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
             <Link to="/forgot/password">
                 <Typography>Forgot password?</Typography>
