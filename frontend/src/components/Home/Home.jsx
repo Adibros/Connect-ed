4f4c4276558cde7cb8d2afe4e3f24c25f1111398
Adibros <86347816+Adibros@users.noreply.github.com>
2023-06-19 06:40:10 +0530

    creating home page nd post ui

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import "./Home.css"
+import User from '../User/User'
+import avatarimg from '../../avatar.jpg'
+import Post from '../Post/Post'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="homeleft">
+        <Post 
+        postImage="https://wallpapercave.com/dwp1x/wp6819375.jpg"
+        ownerName="Aditya"
+        caption="This is sample post"
+        />
+      </div>
+
+      <div className="homeright">
+
+        <User
+              userId = {"user._id"}
+              name={"Aditya"}
+              avatar={avatarimg}
+        />
+
+      </div>
+    </div>
   )
 }
 
